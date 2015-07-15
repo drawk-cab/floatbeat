57f620bfb6440804bfa46d375fe5f4f92036f1af
dave <dave@colourcountry.net>
2015-07-15 21:11:15 +0100

    simple high/low pass filter example refresh examples to work with latest d4 version

--- imports.go
+++ imports.go
@@ -5,21 +5,21 @@ var IMPORTS = map[string]string{
     "scale": `
         :ut 220;
         :A  [ut]Hz;
-        :Bb [ut#]Hz;
-        :B  [ut##]Hz;
-        :C  [ut###]Hz;
-        :Db [ut####]Hz;
-        :D  [ut#####]Hz;
-        :Eb [ut######]Hz; 
-        :E  [ut#######]Hz;
-        :F  [ut########]Hz;
-        :Gb [ut#########]Hz;
-        :G  [ut##########]Hz;
-        :Ab [ut###########]Hz;
+        :Bb [ut ♯]Hz;
+        :B  [ut ♯♯]Hz;
+        :C  [ut ♯♯♯]Hz;
+        :Db [ut ♯♯♯♯]Hz;
+        :D  [ut ♯♯♯♯♯]Hz;
+        :Eb [ut ♯♯♯♯♯♯]Hz; 
+        :E  [ut ↑ ♭♭♭♭♭]Hz;
+        :F  [ut ↑ ♭♭♭♭]Hz;
+        :Gb [ut ↑ ♭♭♭]Hz;
+        :G  [ut ↑ ♭♭]Hz;
+        :Ab [ut ↑ ♭]Hz;
     `,
 
-    "timing": `
-        :beats (loop-count freq -- beat-age beat-num)
+    "divisions": `
+        :divisions (loop-count freq -- beat-age beat-num)
             1 dmod rot mod
         ;
     `,
